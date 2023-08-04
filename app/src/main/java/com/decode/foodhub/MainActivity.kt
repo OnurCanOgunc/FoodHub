8b40e3fab54193381ff26035e9386d6937a46086
Onur Can Öğünç <ogunc606@gmail.com>
2023-08-04 11:42:00 +0300

    Added SplashScreen

--- app/src/main/java/com/decode/foodhub/MainActivity.kt
+++ app/src/main/java/com/decode/foodhub/MainActivity.kt
@@ -2,10 +2,13 @@ package com.decode.foodhub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(12000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
